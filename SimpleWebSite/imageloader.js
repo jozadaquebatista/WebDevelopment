dc8f2e2e17c8c6a9903508f1d784b224936b998d
jozadaquebatista <jozadaquebatista@gmail.com>
2015-05-23 00:29:53 -0300

    A simple website that I did for my university couse.

---
+++ SimpleWebSite/imageloader.js
@@ -0,0 +1,7 @@
+<script language="javascript">
+function imageLoad(img_address)
+{
+	newpopupWindow = window.open ('', 'Esctec::Cadeiras', "width=600 height=600");
+	newpopupWindow.document.write ("<img src='" + img_address +"'width=500>");
+} 
+</script>
